07765a03c2407abf3550b9c43ebc8bd3e26b17fd
Akif Fazil GUVEN <akiff.fazill@gmail.com>
2025-02-12 00:23:15 +0300

    initial commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,27 @@
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Firebase app'i başlat
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Auth ve Firestore servislerini başlat
 const auth = getAuth();
@@ -40,4 +48,4 @@ if (process.env.NEXT_PUBLIC_USE_FIREBASE_EMULATOR === "true") {
   );
 }
 
-export { auth, db };
+export { auth, db, analytics };
